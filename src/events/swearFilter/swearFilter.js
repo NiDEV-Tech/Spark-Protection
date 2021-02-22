2e658eb716e19c352ff7dd83af47f472f80754ba
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2021-02-22 19:50:42 +0100

    Vytvořen event pro nadávání

---
+++ src/events/swearFilter/swearFilter.js
@@ -0,0 +1,18 @@
+// https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-messageDelete
+const BaseEvent = require('../../utils/structures/BaseEvent');
+module.exports = class MessageDeleteEvent extends BaseEvent {
+  constructor() {
+    super('message');
+  }
+  
+  async run(client, message) {
+
+    let swear = ["tydix","idiot","dement","kokot","kripl","kurva","piča",""];
+
+    if (swear.some(word => message.content.toLowerCase().includes(word))) {
+
+      message.delete(swear);
+
+    }
+  }
+}
